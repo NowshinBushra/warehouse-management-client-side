ca6ebf093eb1be0457325ebb97a615c0bae1a182
NowshinBushra <nowshin.bushra399@gmail.com>
2022-05-06 15:49:43 +0600

    set environment variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,13 +7,15 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOKkYBxqYODEkw2e7ZAh39Eqej6P4Cvfk",
-  authDomain: "vehico-mart-services.firebaseapp.com",
-  projectId: "vehico-mart-services",
-  storageBucket: "vehico-mart-services.appspot.com",
-  messagingSenderId: "875841972553",
-  appId: "1:875841972553:web:4973aac4e926cc15cb9298"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
